67cda146f30ef21954095047001c99c508fec680
danrleywillyan <danrleywillian@gmail.com>
2016-03-17 21:17:46 -0300

    Classe principal

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String args[]){
+        System.out.println("SICATROLI");
+        System.out.println("Licensa GPL Versão 3");
+        System.out.println("Sistema para troca de livros");
+    }
+}
