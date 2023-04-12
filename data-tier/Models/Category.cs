605fc16d2c0662aeb796f2101cd298e6c4a0fef9
danrleypereira <danrleywillian@gmail.com>
2023-04-12 05:51:09 -0300

    Modelos e Migrations iniciais

---
+++ data-tier/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace dal.Models;
+public class Category
+{
+    public int Id { get; set; }
+    public string Genre { get; set; }
+    public string Pages { get; set; }
+    public string Rarity { get; set; }
+    public string Conservation { get; set; }
+    public string Price { get; set; }
+}
---
+++ data-tier/Models/Institution.cs
@@ -0,0 +1,5 @@
+public class Institution
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
